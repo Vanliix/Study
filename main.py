f2b1b0c52e0ba82eb0b20513d62af77888e8bc36
”Vanliix”~ <kortoet.geruyno@gmail.com>
2022-10-15 21:39:43 +0300

    Feat(function): add a new function ball volume

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from math import pi
 def convert():
     print("Choose one:")
     print("1. task")
+    print("2. fuck")
     number = input()
-    if number not in ("1"):
+    if number not in ("1" , "2"):
         print("ты даун")
     else:
         if number == "1":
             print((11 * (2 ** 2)) - ((13 / 4) + 7))
-
+        elif number == "2":
+            print((3 * 36 / (4 * pi)) ** (1/3))
 convert()
 while True:
     flag = input("One more time? [y/n]: ")
